68ac0458a10e52034ceb4a1aff83de04e1db77ab
DevEmmy <eolaosebikan60@gmail.com>
2022-06-02 15:03:08 +0100

    try out be

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = require('express').Router();
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const blogRouter = require('./router/blogRouter')
@@ -13,31 +14,32 @@ require('dotenv').config()
 const { mongoose } = require('mongoose')
 
 const app = express();
-app.use(cors())
+// app.use(cors())
 
 
-const uri = process.env.DB_URI
-// "mongodb://localhost:27017/kde"
+// const uri = process.env.DB_URI
+// // "mongodb://localhost:27017/kde"
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-app.use(bodyParser.json({limit:"30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit:"30mb", extended: false}));
+// mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+// app.use(bodyParser.json({limit:"30mb", extended: true}));
+// app.use(bodyParser.urlencoded({limit:"30mb", extended: false}));
 
-const connection = mongoose.connection
-connection.once('open', ()=>{console.log('Database running Successfully')})
+// const connection = mongoose.connection
+// connection.once('open', ()=>{console.log('Database running Successfully')})
 
-app.use(bodyParser.json({limit:"30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit:"30mb", extended: false}));
+// app.use(bodyParser.json({limit:"30mb", extended: true}));
+// app.use(bodyParser.urlencoded({limit:"30mb", extended: false}));
 
-// Routes
-app.use('/blog',blogRouter)
-app.use('/category', categoryRouter)
-app.use('/listing', listingRouter)
-app.use('/location', locationRouter)
-app.use('/message', messageRouter)
-app.use('/site-media', siteMediaRouter)
-app.use('/sub-category', subCategoryRouter)
-app.use('/auth', userRouter)
+// // Routes
+// app.use('/blog',blogRouter)
+// app.use('/category', categoryRouter)
+// app.use('/listing', listingRouter)
+// app.use('/location', locationRouter)
+// app.use('/message', messageRouter)
+// app.use('/site-media', siteMediaRouter)
+// app.use('/sub-category', subCategoryRouter)
+// app.use('/auth', userRouter)
+// app.send("<h1>Hello World</h1>")
 
 
 const port = process.env.PORT || 4444;
